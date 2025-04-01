9b0e937d5489c622d4259b306cc04c263d691bc6
NikTishkov <nik.tishkov25@gmail.com>
2025-04-01 21:26:17 +0700

    Variables + 4

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,11 @@ public class Main {
             var dog = 8.0;
             var cat = 3.6;
             var paper = 763789;
-
+            dog = dog + 4;
+            cat = cat + 4;
+            paper = paper + 4;
             System.out.println(dog);
-            System.out.println(paper);
             System.out.println(cat);
+            System.out.println(paper);
     }
 }
