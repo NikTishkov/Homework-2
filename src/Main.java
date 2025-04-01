f6ad64a88335357470f640f6e5e71c7aa13900a5
NikTishkov <nik.tishkov25@gmail.com>
2025-04-01 21:59:15 +0700

    Task 7

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,7 @@ public class Main {
     public static void main(String[] args) {
             var boxer1 = 78.2;
             var boxer2 = 82.7;
-            var total_weight = boxer1 + boxer2;
-            var weight_difference = boxer2 - boxer1;
-            System.out.println(total_weight);
-            System.out.println(weight_difference);
+            var remainder = boxer2 % boxer1;
+            System.out.println(remainder);
     }
 }
